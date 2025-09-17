3091a1807d3e0e444b171cf93abb8d6f1a115b9d
Chokladglasyr <lim_ida@live.com>
2025-09-17 15:42:53 +0200

    added notifications

--- App.tsx
+++ App.tsx
@@ -7,10 +7,12 @@ import {
 import { registerRootComponent } from "expo";
 
 import * as Notifications from "expo-notifications";
+import { useEffect, useMemo, useState } from "react";
 import { useColorScheme } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AuthStack from "./navigation/AuthStack";
 import MainStack from "./navigation/MainStack";
+import { registerForPushNotificationsAsync } from "./utils/notifications";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -21,10 +23,36 @@ Notifications.setNotificationHandler({
   }),
 });
 
-
 export default function App() {
   const colorScheme = useColorScheme();
   const isLoggedIn = true;
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+    useMemo
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => {console.log("min notis-token: ", token);setExpoPushToken(token ?? "")})
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("notis recieved", notification)
+        setNotification(notification);
+      }
+    );
+
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
 
   return (
     <SafeAreaProvider>
